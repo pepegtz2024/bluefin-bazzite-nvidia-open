65f021aeb569b8acbeea312fdf2f637d94794219
pepegtz2024 <122509632+pepegtz2024@users.noreply.github.com>
2025-05-29 22:50:03 -0600

    Create mcpelauncher.sh

---
+++ build_files/mcpelauncher.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo dnf5 -y install libuv-devel libzip-devel protobuf-devel protobuf-compiler qt5-qtbase-devel qt5-qtwebengine-devel qt5-qtdeclarative-devel qt5-qtsvg-devel qt5-qtquickcontrols qt5-qtquickcontrols2
+git clone --recursive https://github.com/minecraft-linux/mcpelauncher-ui-manifest.git mcpelauncher-ui
+cd mcpelauncher-ui && mkdir -p build && cd build
+cmake ..
+make -j$(getconf _NPROCESSORS_ONLN)
