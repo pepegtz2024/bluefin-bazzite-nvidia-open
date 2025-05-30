d1927bcc803e07a50d3845c2beac4aac6925697d
pepegtz2024 <122509632+pepegtz2024@users.noreply.github.com>
2025-05-29 20:48:17 -0600

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf copr -y enable g3tchoo/prismlauncher
+dnf install -y lutris steam waydroid prismlauncher
 
 # Use a COPR Example:
 #
@@ -18,7 +19,8 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+dnf -y copr disable g3tchoo/prismlauncher
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
